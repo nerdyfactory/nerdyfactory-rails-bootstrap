12d42cc56445b39f3739e85c3b7d0e2a2742af60
JonghunYu <yuzong@naver.com>
2017-04-12 16:59:41 +0900

    added some configuration changes for spec

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,8 @@
 FactoryGirl.define do
   factory :user do
-    
+    password = Faker::Internet.password
+    email Faker::Internet.email
+    password password
+    password_confirmation password
   end
 end
